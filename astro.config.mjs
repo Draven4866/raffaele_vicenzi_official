c5edb740062241c374cbdd665fcce28926796bb8
Draven4866 <252122135+Draven4866@users.noreply.github.com>
2026-01-03 13:57:24 +0100

    Update astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+const repo = "raffaele_vicenzi_official";
+const isProd = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+  site: "https://draven4866.github.io",
+  base: isProd ? `/${repo}/` : "/",
+});
