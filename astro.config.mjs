6d17f1e579c7edfd18f413e4c4de7d4027797dae
Draven4866 <252122135+Draven4866@users.noreply.github.com>
2026-01-05 09:32:34 +0100

    Fix Astro base for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://draven4866.github.io',
-  base: '/raffaele_vicenzi_official',
-  trailingSlash: 'always',
+  site: 'https://www.raffaelevicenzi.it',
+  // IMPORTANT: niente base, oppure base: '/'
 });
